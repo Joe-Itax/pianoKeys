16b00b70c2c3cc8dcc77bda196b49de6943b5ae4
Joseph ITAKALA <josephitakala18@gmail.com>
2023-08-07 16:59:23 +0100

    -

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
 // The keys and notes variables store the piano keys
-const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+const keys = [
+  'c-key',
+  'd-key',
+  'e-key',
+  'f-key',
+  'g-key',
+  'a-key',
+  'b-key',
+  'high-c-key',
+  'c-sharp-key',
+  'd-sharp-key',
+  'f-sharp-key',
+  'g-sharp-key',
+  'a-sharp-key',
+];
 const notes = [];
-keys.forEach(function(key) {
+keys.forEach(function (key) {
   notes.push(document.getElementById(key));
-})
-//Hello
+});
+notes.forEach(function (key) {
+  console.log(key);
+});
+// console.table(notes);
 // Write named functions that change the color of the keys below
+/*
+const keyPlay = (event) => {
+  event.target.style.backgroundColor = '#2176ff';
+  console.log('hello');
+};
 
+const keyReturn = (event) => {
+  event.target.style.backgroundColor = '';
+  console.log('Nohello');
+};
 
 // Write a named function with event handler properties
+const eventAssign = function (note) {
+  note.onmousedown = function () {
+    keyPlay(event);
+  };
 
+  note.onmouseup = function () {
+    keyReturn(event);
+  };
+};
 
 // Write a loop that runs the array elements through the function
-
-
+notes.forEach(eventAssign);
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
 let nextTwo = document.getElementById('second-next-line');
@@ -30,15 +63,12 @@ startOver.hidden = true;
 
 // Write anonymous event handler property and function for the first progress button
 
-
 // Write anonymous event handler property and function for the second progress button
-
 
 // Write anonymous event handler property and function for the third progress button
 
-
 // This is the event handler property and function for the startOver button
-startOver.onclick = function() {
+startOver.onclick = function () {
   nextOne.hidden = false;
   startOver.hidden = true;
   document.getElementById('word-one').innerHTML = 'HAP-';
@@ -53,4 +83,5 @@ startOver.onclick = function() {
   document.getElementById('letter-note-five').innerHTML = 'C';
   document.getElementById('word-six').innerHTML = 'YOU!';
   document.getElementById('letter-note-six').innerHTML = 'B';
-}
+};
+*/
