5535fce2da6cdd18065f2b1ae1378590f3cc8cb5
Joseph-Itakala <123893709+Joseph-Itakala@users.noreply.github.com>
2023-08-07 12:51:27 +0000

    add : add files index.html, style.css, index.js and main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
-keys.forEach(function(key){
+keys.forEach(function(key) {
   notes.push(document.getElementById(key));
 })
-
+//Hello
 // Write named functions that change the color of the keys below
 
 
@@ -26,7 +26,7 @@ let lastLyric = document.getElementById('column-optional');
 // These statements are "hiding" all the progress buttons, but the first one
 nextTwo.hidden = true;
 nextThree.hidden = true;
-startOver.hidden= true;
+startOver.hidden = true;
 
 // Write anonymous event handler property and function for the first progress button
 
@@ -41,7 +41,7 @@ startOver.hidden= true;
 startOver.onclick = function() {
   nextOne.hidden = false;
   startOver.hidden = true;
-   document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('word-one').innerHTML = 'HAP-';
   document.getElementById('letter-note-one').innerHTML = 'G';
   document.getElementById('word-two').innerHTML = 'PY';
   document.getElementById('letter-note-two').innerHTML = 'G';
